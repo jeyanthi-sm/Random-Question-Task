8d0591fec6864e7aa856ab2eb007ad4dcc124e96
Meenashi <jeyanthi_sm@yahoo.com>
2023-05-26 06:11:38 +0100

    feat:Inital Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,53 +1,101 @@
-import StaticMath from '../components/StaticMath/StaticMath'
-import MathInput from '../components/MathInput/MathInput';
-import { useState } from 'react';
-import '../public/styles/globals.css'
-import { evaluateTex } from 'tex-math-parser';
+import StaticMath from "../components/StaticMath/StaticMath";
+import MathInput from "../components/MathInput/MathInput";
+import { useState } from "react";
+import "../public/styles/globals.css";
+import { evaluateTex } from "tex-math-parser";
 
-export default function App({}){
+export default function App({}) {
+  const [memory, setMemory] = useState({});
+  const [solutionShown, setSolutionShown] = useState(false);
 
-    const [memory, setMemory] = useState({});
-    const [solutionShown, setSolutionShown] = useState(false);
+  function addToMemory(newValue) {
+    setMemory((prev) => {
+      return { ...prev, ...newValue };
+    });
+  }
 
-    function addToMemory(newValue){
-        setMemory((prev)=>{
-            return {...prev, ...newValue}
-        });
-    }
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div
+        style={{
+          maxWidth: "800px",
+          width: "calc(100vw - 40px)",
+          marginTop: "50px",
+        }}
+      >
+        {/* <StaticMath latex={`\\text{The <StaticMath /> component can be used to write text inline with latex equations: } x^2 + 3x - 2`} />
+                <StaticMath latex={`\\text{Generate a random question and display it here.}`} /> */}
+        <StaticMath latex={`\\text{Some Random Question}`} />
 
-    return(
-        <div style={{display:'flex', justifyContent:'center'}}>
-            <div style={{maxWidth:'800px', width:'calc(100vw - 40px)', marginTop:'50px'}}>
-                <StaticMath latex={`\\text{The <StaticMath /> component can be used to write text inline with latex equations: } x^2 + 3x - 2`} />
-                <StaticMath latex={`\\text{Generate a random question and display it here.}`} />
-                <br/>
-                <br/>
-                {solutionShown ? <StaticMath latex={`\\text{Display the solution here}`} /> : ''}
-                <br/>
-                <br/>
-                <MathInput buttons={['power', 'times']} markingFunction={markingFunction} memKey='mathinput1' memory={memory} setMemory={addToMemory} placeholder="Type your answer here!"/>
-                <br/>
-                <br/>
-                <button onClick={()=>{setMemory((prev)=>{return{...prev, feedbackShown:true}})}}>Check Answer</button>
-                <br/>
-                {!solutionShown ? <button style={{marginTop:'20px'}} onClick={()=>{setSolutionShown(true)}}>Show Solution</button> : ''}
-                {solutionShown ? <button style={{marginTop:'20px'}} onClick={()=>{setSolutionShown(false)}}>Hide Solution</button> : ''}
-            </div>
-        </div>
-    );
+        <br />
+        <br />
+        {solutionShown ? (
+          <StaticMath latex={`\\text{Display the solution here}`} />
+        ) : (
+          ""
+        )}
+        <br />
+        <br />
+        <MathInput
+          buttons={["power", "times"]}
+          markingFunction={markingFunction}
+          memKey="mathinput1"
+          memory={memory}
+          setMemory={addToMemory}
+          placeholder="Type your answer here!"
+        />
+        <br />
+        <br />
+        <button
+          onClick={() => {
+            setMemory((prev) => {
+              return { ...prev, feedbackShown: true };
+            });
+          }}
+        >
+          Check Answer
+        </button>
+        <br />
+        {!solutionShown ? (
+          <button
+            style={{ marginTop: "20px" }}
+            onClick={() => {
+              setSolutionShown(true);
+            }}
+          >
+            Show Solution
+          </button>
+        ) : (
+          ""
+        )}
+        {solutionShown ? (
+          <button
+            style={{ marginTop: "20px" }}
+            onClick={() => {
+              setSolutionShown(false);
+            }}
+          >
+            Hide Solution
+          </button>
+        ) : (
+          ""
+        )}
+      </div>
+    </div>
+  );
 }
 
-function markingFunction(userInput){
-    let inputValue;
-    try{
-        //the evaluateTex function takes a latex string as an input and returns the evaluation as a javascript number
-        inputValue = evaluateTex(userInput).evaluated;
-    }catch{
-        return 0;
-    }
-    if(inputValue === 5){
-        return 1
-    }else{
-        return 0;
-    }
+function markingFunction(userInput) {
+  let inputValue;
+  try {
+    //the evaluateTex function takes a latex string as an input and returns the evaluation as a javascript number
+    inputValue = evaluateTex(userInput).evaluated;
+  } catch {
+    return 0;
+  }
+  if (inputValue === 5) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
